4ef9de8a44637aaee98a4ec446778f0416e7d9ab
Алексей Даниленко <a_danilenko@controlsystems.ru>
2025-08-10 01:00:02 +0300

    test docker + actions fix 1

--- main.py
+++ main.py
@@ -18,6 +18,5 @@ if __name__ == '__main__':
     application.add_handler(start_handler)
 
     print('🚀🚀🚀 TG BOT HAS STARTED 🚀🚀🚀')
-    print('🚀🚀🚀 TG BOT HAS STARTED 🚀🚀🚀')
 
     application.run_polling()
